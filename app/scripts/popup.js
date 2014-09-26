5450f9f1c225483d95b2bdcd523d0b01f540fdab
Tyler Garrett <tyler@tgrrtt.com>
2014-09-25 20:55:58 -0700

    Added jquery & yo post request

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+//console.log('\'Allo \'Allo! Popup');
+$(function(){
+  $('#yo-button').on('click', function(){
+    var username = $('#username').val();
+    $.post('http://api.justyo.co/yo/', {
+      api_token: '410958b8-b7da-3856-65d9-d5ee2d934559',
+      username: username
+    });
+  });
+});
