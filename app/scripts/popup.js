a557314731908867fc1fa9e73343bdf1ffc41cd7
John Heroy <johnheroy@gmail.com>
2014-09-25 21:20:48 -0700

    Just Yo :bomb:

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,9 +1,12 @@
 $(function(){
-  $('#yo-button').on('click', function(){
+  $('#yo-form').submit(function(event){
+    event.preventDefault();
     var username = $('#username').val();
     $.post('http://api.justyo.co/yo/', {
       api_token: '410958b8-b7da-3856-65d9-d5ee2d934559',
       username: username
+    }, function(){
+      $('#username').val('type a yo username');
     });
   });
 });
