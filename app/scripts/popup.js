d250e82545bf039fa9b1e646e30cdfc581e2e854
Tyler Garrett <tyler@tgrrtt.com>
2014-09-26 20:47:03 -0700

    Added rudimentary url sending

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,15 +1,46 @@
 $(function(){
-  $('#yo-form').submit(function(event){
-    event.preventDefault();
-    var username = $('#username').val();
-    $.post('http://api.justyo.co/yo/', {
-      api_token: secretkey,
-      username: username
-    }, function(){
-      $('#username').val('');
-      $('#err-msg').hide();
-    }).fail(function(){
-      $('#err-msg').show();
-    });
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    handleForm(tabs[0].url);
   });
+
+  function handleForm(tablink) {
+    //alert(tablink);
+    $("#link").val(tablink)
+    var url = $('#link').val() || 'http://optional-link.com';
+
+    $('#yo-form').submit(function(event){
+      event.preventDefault();
+      var username = $('#username').val();
+      $.post('http://api.justyo.co/yo/', {
+        api_token: secretkey,
+        link: url,
+        username: username
+      }, function(){
+        $('#username').val('');
+        $('#err-msg').hide();
+      }).fail(function(){
+        $('#err-msg').show();
+      });
+    });
+  }
+
+
+
+  // $("#link").val(tabUrl)
+  // var url = $('#link').val() || 'http://optional-link.com';
+
+  // $('#yo-form').submit(function(event){
+  //   event.preventDefault();
+  //   var username = $('#username').val();
+  //   $.post('http://api.justyo.co/yo/', {
+  //     api_token: secretkey,
+  //     link: url,
+  //     username: username
+  //   }, function(){
+  //     $('#username').val('');
+  //     $('#err-msg').hide();
+  //   }).fail(function(){
+  //     $('#err-msg').show();
+  //   });
+  // });
 });
