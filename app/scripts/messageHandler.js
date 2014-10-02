0ac1f6f21979cad5da2e89d45ef9d8625219b135
Tyler Garrett <tyler@tgrrtt.com>
2014-10-01 22:12:33 -0700

    Content security policy allows for connection to firebase

--- app/scripts/messageHandler.js
+++ app/scripts/messageHandler.js
@@ -1,5 +1,6 @@
 $(function(){
   var firebase = new Firebase('https://yo-chrome.firebaseio.com/');
+  // alert(firebase);
   firebase.child(MY_USERNAME).on('child_added', function(snapshot){
     var data = snapshot.val();
     console.log(data['yo from']);
